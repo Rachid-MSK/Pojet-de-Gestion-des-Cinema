84a657513ca3e62866c531903dea3528f8f89ace
MESKY Rachid <rachidmesky@gmail.com>
2021-01-08 18:39:54 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CinemaComponent } from './cinema/cinema.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CinemaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: "cinema", component: CinemaComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
